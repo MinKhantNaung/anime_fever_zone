0cbfa698fdb69c29a12e2170bf43613c1a6f31b2
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-07-07 22:24:43 +0630

    Update; remove search btn

--- resources/views/livewire/components/nav-bar.blade.php
+++ resources/views/livewire/components/nav-bar.blade.php
@@ -2,7 +2,8 @@
     <div class="w-full container mx-auto flex flex-nowrap items-center justify-between">
 
         <nav>
-            <ul class="flex items-center justify-between font-bold text-sm text-white pl-0 sm:pl-6 uppercase no-underline">
+            <ul
+                class="flex items-center justify-between font-bold text-sm text-white pl-2 sm:pl-6 uppercase no-underline">
                 <li>
                     <a wire:navigate.hover href="{{ route('home') }}" class="hover:text-gray-200 hover:underline px-4">
                         <img src="{{ asset('favicon.ico') }}" alt="anime fever zone logo" class="w-9">
@@ -14,14 +15,6 @@
         </nav>
 
         <div class="flex items-center text-lg no-underline text-white pr-0 sm:pr-6 pl-5 sm:pl-0">
-            <!-- Search -->
-            <button>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="w-8 h-8">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                </svg>
-            </button>
 
             @guest
                 <a wire:navigate.hover href="{{ route('login') }}" class="px-2 hover:text-gray-200 hover:underline">
