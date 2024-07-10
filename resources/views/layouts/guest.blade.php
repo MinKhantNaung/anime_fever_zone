f7776a64d5a22b0c5a758920bdb1d205a645f294
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-07-10 17:16:03 +0630

    Fix: remove bunny fonts from guest layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -9,10 +9,6 @@
 
         <meta name="robots" content="noindex, follow">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
