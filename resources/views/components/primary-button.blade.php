6a13ccaab828f3f648e288683e8659362c935fa8
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-08-17 14:52:02 +0630

    Update: primary button colors

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-[#9926f0] border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-[#d122e3] focus:bg-[#9926f0] active:bg-[#d122e3] focus:outline-none focus:ring-2 focus:ring-[#d122e3] focus:ring-offset-2 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
