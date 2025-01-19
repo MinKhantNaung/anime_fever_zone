a9405c9cd9b5be58dc7f882f3b001b81d45852c3
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2025-01-19 22:46:46 +0630

    Update: use canva mouse-move-effect

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -5,9 +5,12 @@
  */
 
 import axios from 'axios';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+import './magicmouse';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
