7242fcf17880c319e5d1f9619df07f87e378581b
Min Khant Naung <minkhantnaungkzw@gmail.com>
2025-02-06 16:55:17 +0630

    Fix: remove commentify package

--- config/app.php
+++ config/app.php
@@ -169,7 +169,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\VoltServiceProvider::class,
-        Usamamuneerchaudhary\Commentify\Providers\CommentifyServiceProvider::class
     ])->toArray(),
 
     /*
