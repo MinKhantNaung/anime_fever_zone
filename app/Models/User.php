e6c802d6ad1bb98ac228cdbe2c66ccfb0080ff0f
Min Khant Naung <minkhantnaungkzw@gmail.com>
2025-02-06 17:53:26 +0630

    Add: comment feature logics to Livewire Classes

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,8 +3,8 @@
 namespace App\Models;
 
 use App\Traits\HasUserAvatar;
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\Contracts\HasApiTokens;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
