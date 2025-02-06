c391ce420ee4d48e26c52afd9c22d3e6aa903830
Min Khant Naung <minkhantnaungkzw@gmail.com>
2025-02-06 17:34:58 +0630

    Add: finish Livewire comment feature

---
+++ app/Livewire/CommentFeature/Like.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\CommentFeature;
+
+use Livewire\Component;
+
+class Like extends Component
+{
+    public function render()
+    {
+        return view('livewire.comment-feature.like');
+    }
+}
