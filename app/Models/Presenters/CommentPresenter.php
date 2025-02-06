70877a511e61a139ecbfd618d7b5ab9622da1957
Min Khant Naung <minkhantnaungkzw@gmail.com>
2025-02-06 17:13:05 +0630

    Add: comment, comment_like models and migrations

---
+++ app/Models/Presenters/CommentPresenter.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models\Presenters;
+
+use App\Models\User;
+use App\Models\Comment;
+use Illuminate\Support\HtmlString;
+
+final class CommentPresenter
+{
+    /**
+     * @var Comment
+     */
+    public Comment $comment;
+
+    public function __construct(Comment $comment)
+    {
+        $this->comment = $comment;
+    }
+
+    public function markdownBody(): HtmlString
+    {
+        return new HtmlString(app('markdown')->convertToHtml($this->comment->body));
+    }
+
+    public function relativeCreatedAt(): mixed
+    {
+        return $this->comment->created_at->diffForHumans();
+    }
+
+    public function replaceUserMentions($text): array|string
+    {
+        preg_match_all('/@([A-Za-z0-9_]+)/', $text, $matches);
+        $usernames = $matches[1];
+        $replacements = [];
+
+        foreach ($usernames as $username) {
+            $user = User::where('name', $username)->first();
+            if ($user) {
+                $userRoutePrefix = config('commentify.users_route_prefix', 'users');
+                $replacements['@' . $username] = '<a href="/' . $userRoutePrefix . '/' . $username . '">@' . $username .
+                    '</a>';
+            } else {
+                $replacements['@' . $username] = '@' . $username;
+            }
+        }
+
+        return str_replace(array_keys($replacements), array_values($replacements), $text);
+    }
+}
---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\Models\Comment;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CommentFactory extends Factory
+{
+    protected $model = Comment::class;
+
+    public function definition(): array
+    {
+        return [
+            'body' => fake()->text,
+            'user_id' => function () {
+                return User::factory()->create()->id;
+            },
+            'parent_id' => null,
+            'commentable_type' => '\ArticleStub',
+            'commentable_id' => 1,
+            'created_at' => now()
+        ];
+    }
+}
