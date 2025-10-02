5b98697e5d419baa82ed907f2d1e28da8beb2868
Min Khant Naung <minkhantnaungkzw@gmail.com>
2025-10-02 06:39:47 +0000

    Add: offline mode component

---
+++ resources/views/livewire/offline.blade.php
@@ -0,0 +1,14 @@
+<div wire:offline
+    class="fixed bottom-4 left-1/2 -translate-x-1/2
+            inline-flex items-center
+            bg-red-100 border border-red-400 text-red-700
+            px-4 py-2 rounded-lg shadow-lg
+            text-sm font-medium animate-pulse z-50
+            whitespace-nowrap w-auto max-w-max">
+    <svg class="w-5 h-5 text-red-600 flex-shrink-0 inline-block" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+        stroke="currentColor" stroke-width="2">
+        <path stroke-linecap="round" stroke-linejoin="round"
+            d="M12 9v2m0 4h.01M21 12c0-4.97-4.03-9-9-9S3 7.03 3 12s4.03 9 9 9 9-4.03 9-9z" />
+    </svg>
+    <span class="ml-2">You are now offline.</span>
+</div>
