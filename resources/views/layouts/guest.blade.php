932a0793724fef69410e8e01ed6d8d47d3122c8e
Min Khant Naung <minkhantnaungkzw@gmail.com>
2026-01-22 08:32:24 +0000

    refactor: just format in guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,41 +1,44 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <meta name="robots" content="noindex, follow">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <meta name="robots" content="noindex, follow">
 
-        <!-- Livewire styles -->
-        @livewireStyles
-    </head>
-    <body class="font-sans text-gray-900 antialiased">
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-        <livewire:components.nav-bar />
+    <!-- Livewire styles -->
+    @livewireStyles
+</head>
 
-        <livewire:offline />
+<body class="font-sans text-gray-900 antialiased">
 
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-5 bg-gray-100">
-            <div>
-                <a wire:navigate.hover href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
+    <livewire:components.nav-bar />
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
-            </div>
+    <livewire:offline />
+
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-5 bg-gray-100">
+        <div>
+            <a wire:navigate.hover href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
         </div>
 
-        <!-- AI Chat Box -->
-        <livewire:components.ai-chat-box />
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            {{ $slot }}
+        </div>
+    </div>
 
-        @livewireScripts(['defer' => true])
-    </body>
+    <!-- AI Chat Box -->
+    <livewire:components.ai-chat-box />
+
+    @livewireScripts(['defer' => true])
+</body>
+
 </html>
